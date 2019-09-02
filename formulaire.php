2a3cf2690e200f9feefcaad4f59efe5d45bcc525
Tony-Gr <garcia.tony84@gmail.com>
2019-09-02 12:29:54 +0200

    maj_formulaire

--- formulaire.php
+++ formulaire.php
@@ -1,22 +1,44 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
     <form action="">
-        <select name="Appart" id="appart-select">
-            <option value="appart1"> Appartement 1</option>
-        </select>
-        <input type="email" name="email" placeholder="exemple@gmail.com">
-        <input type="name" name="name" placeholder="Name">
-        <input type="surname" name="surname" placeholder="Surname">
-        <input type="text" name="adresse" placeholder="Address">
-        <input type="tel" name="tel" placeholder="06 xx xx xx xx">
-        <input type="submit" name="submit" placeholder="Valid">
-    </form>
+        <form>
+            <div class="form-group">
+                <label for="appart-select"></label>
+                <select name="Appart" id="appart-select">
+                    <option value="appart1"> Appartement 1</option>
+                </select>
+            </div>
+            <div class="form-group">
+                <label for="exampleInputEmail1"></label>
+                <input type="email" name="email" placeholder="exemple@gmail.com" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp">
+            </div>
+            <div class="form-group">
+                <label for="name"></label>
+                <input type="name" name="name" placeholder="Name" class="form-control" id="name">
+            </div>
+            <div class="form-group">
+                <label for="surname"></label>
+                <input type="surname" name="surname" placeholder="Surname" id="surname" class="form-control">
+            </div>
+            <div class="form-group">
+                <label for="address"></label>
+                <input type="text" name="adresse" placeholder="Address" id="address" class="form-control">
+            </div>
+                <div class="form-group">
+                    <label for="tel"></label>
+                    <input type="tel" name="tel" placeholder="06 xx xx xx xx" id="tel" class="form-control">
+                </div>
+            <button type="submit" class="btn btn-primary">Valid</button>
+        </form>
 </body>
+
 </html>
